e174c545284b6db5c86a6bfa91bbd6924418f03e
DrAl-HFS <al@INKCAP.connect>
2019-02-04 21:44:17 +0000

    Basic performance tabulation over neighbourhood / iteration

--- src/diffTest.c
+++ src/diffTest.c
@@ -138,7 +138,7 @@ uint test1 (Test1Res *pR, const Test1Param *pP)
    return(iT);
 } // test1
 
-void redCheck (MMSMVal *pMM, const DiffScalar *pN, const DiffScalar *pA)
+void redCheck (MMSMVal *pMM, const DiffScalar *pN, const DiffScalar *pA, uint f)
 {
    RedRes rN, rA;
    SMVal dt;
@@ -148,52 +148,68 @@ void redCheck (MMSMVal *pMM, const DiffScalar *pN, const DiffScalar *pA)
    reducto(&rA, pA, gCtx.org.n1F);
    dt= deltaT();
    //if ((rN.sum != gM) || (rA.sum != gM)) { 
-   printf("sums: N=%G A=%G\n", rN.sum, rA.sum);
+   if (f & 1) { printf("sums: N=%G A=%G\n", rN.sum, rA.sum); }
    pMM->vMin= fmin(rN.mm.vMin, rA.mm.vMin);
    pMM->vMax= fmin(rN.mm.vMax, rA.mm.vMax);
-   printf("MM: N=%G,%G A=%G,%G %Gsec\n", rN.mm.vMin, rN.mm.vMax, rA.mm.vMin, rA.mm.vMax, dt);
+   if (f & 1) { printf("MM: N=%G,%G A=%G,%G %Gsec\n", rN.mm.vMin, rN.mm.vMax, rA.mm.vMin, rA.mm.vMax, dt); }
 } // redCheck
 
 int main (int argc, char *argv[])
 {
-   Test1Param param;
-   Test1Res   res;
    int r= 0;
 
    if (init(&gCtx,1<<8))
    {
       const Index zSlice= gCtx.org.def.z / 2;
       uint iT=0, iN= 0, iA= 0;
-      U8 nHoods[]={6,14,18,26};
+      U8 nHoods[4]={6,14,18,26};
+      Test1Param param;
+      Test1Res   res[4][5], *pR;
 
       //test(&(gCtx.org));
       //pragma acc set device_type(acc_device_none)
       //initW(gCtx.wPhase[0].w, 0.5, 6, 0); // ***M8***
       //iT= diffProcIsoD3S6M(gCtx.pSR[1], gCtx.pSR[0], &(gCtx.org), (D3S6IsoWeights*)(gCtx.wPhase), gCtx.pM, 100);
 
-      for (int i=0; i<sizeof(nHoods); i++)
+      for (int h=0; h<sizeof(nHoods); h++)
       {
+         pR= res[h];
          param.rD=    0.5;
-         param.nHood= nHoods[i];
-         param.iter=  100;
-         if (iT= test1(&res,&param))
+         param.nHood= nHoods[h];
+         for (int i=20; i<=100; i+= 20)
          {
-            MMSMVal mm;
-
-            printf("diffProc..%u %u iter %G sec (%G msec/iter) Dt=%G sad=%G\n", param.nHood, iT, res.tProc, 1000*res.tProc / iT, res.sr.Dt, res.sr.sad);
-            iN= iT & 1;
-            iA= iN ^ 1;
-            initPhaseAnalytic(gCtx.pSR[iA], &(gCtx.org), 0, gM, res.sr.Dt);
-            redCheck(&mm, gCtx.pSR[iN], gCtx.pSR[iA]);
+            param.iter=  i;
+            iT= test1(pR, &param);
+            if (100 == iT)
             {
-               char path[256];
-               snprintf(path, sizeof(path)-1, "%s/N%02d%s", "rgb", param.nHood, "numerical.rgb");
-               saveSliceRGB(path, gCtx.pSR[iN], 0, zSlice, &(gCtx.org), &mm);
-               snprintf(path, sizeof(path)-1, "%s/N%02d%s", "rgb", param.nHood, "analytic.rgb");
-               saveSliceRGB(path, gCtx.pSR[iA], 0, zSlice, &(gCtx.org), &mm);
-               snprintf(path, sizeof(path)-1, "%s/N%02d%s", "rgb", param.nHood, "sad.rgb");
-               saveSliceRGB(path, gCtx.ws.p, 0, 0, &(gCtx.org), NULL);
+               MMSMVal mm;
+
+               printf("diffProc..%u %u iter %G sec (%G msec/iter) Dt=%G sad=%G\n", param.nHood, iT, pR->tProc, 1000*pR->tProc / iT, pR->sr.Dt, pR->sr.sad);
+               iN= iT & 1;
+               iA= iN ^ 1;
+               initPhaseAnalytic(gCtx.pSR[iA], &(gCtx.org), 0, gM, pR->sr.Dt);
+               redCheck(&mm, gCtx.pSR[iN], gCtx.pSR[iA], 1);
+               {
+                  char path[256];
+                  snprintf(path, sizeof(path)-1, "%s/N%02d%s", "rgb", param.nHood, "numerical.rgb");
+                  saveSliceRGB(path, gCtx.pSR[iN], 0, zSlice, &(gCtx.org), &mm);
+                  snprintf(path, sizeof(path)-1, "%s/N%02d%s", "rgb", param.nHood, "analytic.rgb");
+                  saveSliceRGB(path, gCtx.pSR[iA], 0, zSlice, &(gCtx.org), &mm);
+                  snprintf(path, sizeof(path)-1, "%s/N%02d%s", "rgb", param.nHood, "sad.rgb");
+                  saveSliceRGB(path, gCtx.ws.p, 0, 0, &(gCtx.org), NULL);
+               }
             }
+            pR++;
+         }
+      }
+      printf("NH I\tt\tDt\tSAD\tEfficiency(Dt/t)\n");
+      for (int h=0; h<sizeof(nHoods); h++)
+      {
+         pR= res[h];
+         for (int i=20; i<=100; i+= 20)
+         {
+            printf("%u %u\t%Gsec\t%G\t%G\t%G\n", nHoods[h], i, pR->tProc, pR->sr.Dt, pR->sr.sad, pR->sr.Dt / pR->tProc);
+            pR++;
          }
       }
    }
