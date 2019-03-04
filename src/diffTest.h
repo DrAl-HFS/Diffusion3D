28e81a1cbce3d8c90e05017d70cbcd56fa6dab7b
DrAl-HFS <DrAl.HFS@gmail.com>
2019-03-04 21:26:22 +0000

    Initial commit of dummy header for build compatibility

---
+++ src/diffTest.h
@@ -0,0 +1,20 @@
+// diffTest.h - 3D Diffusion under OpenACC
+// https://github.com/DrAl-HFS/Diffusion3D.git
+// (c) Diffusion3D Project Contributors Jan-Mar 2019
+
+#ifndef DIFF_TEST_H
+#define DIFF_TEST_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif // DIFF_TEST_H
+
+
+
